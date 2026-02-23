ba92e2dafcbc8ddf499669d6be15933625271f99
Tabish2402 <yellowflash2402@gmail.com>
2026-02-23 10:49:21 +0530

    Finalize backend  pages and navigation

--- app/main.py
+++ app/main.py
@@ -11,7 +11,11 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://athelean-x-frontend.vercel.app"],
+    allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://athelean-x-frontend-4qllwsmun-tabish-riyazs-projects.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
